2ec6663d80d740177dad69c25f43512805a738c7
Tommaso Magarotto <Tommymaga01@gmail.com>
2024-12-18 14:31:05 +0100

    Create SimpleStuff.R

---
+++ R/SimpleStuff.R
@@ -0,0 +1,14 @@
+# The function to build a function is: function
+# function (x,y), but we need graph parenthesis { } as is stating that inside the function something is happening
+function(x,y) {
+  z=x+y
+  return(z)
+  }
+#the name should be the same as the title
+SimpleStuff <- function(x,y) {
+  z=x+y
+  return(z)
+  }
+#to check if it works go to R and put it inside the function
+#you can also assign these numbers to some variables and in this way, it is possible to use them in the function
+#take home message: you can build your function, so open sources enable you to use others' function and improve them
